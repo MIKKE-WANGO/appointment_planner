b9cad6e23e7ba82b5955897aebe719e254b7bb03
MIKKE-WANGO <71769643+MIKKE-WANGO@users.noreply.github.com>
2021-12-22 11:58:05 +0300

    b4 adding inline formsets

--- crm1/crm1/accounts/urls.py
+++ crm1/crm1/accounts/urls.py
@@ -2,7 +2,10 @@ from django.urls import path
 from .import views
 
 urlpatterns = [
-    path('', views.home, name='home '),
-    path('customers/', views.customers, name='customers '),
-    path('products/', views.products, name='products '),
+    path('', views.home, name='home'),
+    path('customer/<str:pk>/', views.customer, name='customer'),
+    path('products/', views.products, name='products'),
+    path('create_order/', views.createorder, name='create_order'),
+    path('update_order/<str:pk>/', views.updateorder, name='update_order'),
+    path('delete_order/<str:pk>/', views.deleteorder, name='delete_order'),
 ]
--- crm1/crm1/accounts/views.py
+++ crm1/crm1/accounts/views.py
@@ -1,12 +1,73 @@
+from django.db import reset_queries
 from django.http.response import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http import HttpResponse
+
+
+from .models import * 
+from .forms import*
 # Create your views here.
 def home(request):
-    return render(request, 'accounts/dashboard.html')
+    orders = Order.objects.all()
+    customers = Customer.objects.all()
+
+    total_customers = customers.count()
+    total_orders = orders.count()
+    delivered = orders.filter(status='Delivered').count
+    pending  = orders.filter(status='Pending').count
+
+
+    context = {'orders': orders, 'customers':customers, 'delivered':delivered, 'pending':pending , 'total_customers':total_customers, 'total_orders':total_orders }
+    return render(request, 'accounts/dashboard.html', context)
 
 def products(request):
-    return render(request, 'accounts/products.html')
+    products = Product.objects.all()
+    
+    return render(request, 'accounts/products.html', {'products':products})
 
-def customers(request):
-    return render(request, 'accounts/customers.html')
+def customer(request,pk):
+    customer = Customer.objects.get(id=pk)
+    orders = customer.order_set.all()
+    order_count = orders.count()
+    context = {'customer': customer, 'orders': orders, 'order_count':order_count}
+
+    return render(request, 'accounts/customers.html', context)
+
+
+def createorder(request):
+    form = OrderForm()
+    if request.method=='POST':
+        #save form details
+        form = OrderForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('/')
+
+    context = {'form': form}
+    return render (request, 'accounts/order_form.html', context)
+
+def updateorder(request,pk):
+
+    #get a form filled with a specific order's details
+    order = Order.objects.get(id=pk)
+    form = OrderForm(instance=order)
+
+    #save form details
+    # 'instance= order' prevents it from creating a new order
+    if request.method=='POST':
+        form = OrderForm(request.POST, instance= order)
+        if form.is_valid():
+            form.save()
+            return redirect('/')
+
+    context = {'form': form}
+    return render (request, 'accounts/order_form.html', context)
+
+def deleteorder(request, pk):
+    order = Order.objects.get(id=pk)
+
+    if request.method=='POST':
+        order.delete()
+        return redirect('/')
+    context={"item":order}
+    return render(request, 'accounts/delete.html', context)
